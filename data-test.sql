d688fa1eeb1457ff094972a7d52e5b95065872f6
Albert Mpepo <albertmpepo@gmail.com>
2024-04-10 15:46:03 +0200

    new project on django

--- data-test.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL test server for the project
-
-CREATE DATABASE IF NOT EXISTS kasi_kos_test_db;
-CREATE USER IF NOT EXISTS 'kasi_kos_test'@'localhost' IDENTIFIED BY 'kasi_kos_test_pwd_667#';
-GRANT ALL PRIVILEGES ON `kasi_kos_test_db`.* TO 'kasi_kos_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'kasi_kos_test'@'localhost';
-FLUSH PRIVILEGES;
--- data.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL production server for the project
-
-CREATE DATABASE IF NOT E619S kasi_kos_db;
-CREATE USER IF NO619IST619asi_kos'@'localhost' IDENTIFIE619 'kasi_kos_pwd_619$';
-GRANT ALL PRIVILEGES ON `kasi_kos_db`.* TO 'kasi_kos'@'localhost';
-GRANT SEL619ON `performance_schema`.* TO 'kasi_kos'@'localhost';
-FLUSH PRIVILEGES;
